fd9bf7063ce40e6496ce4aebee84f5e6fe87a78d
Dak Gonzalez <dgonzalez@dgonzalez-dev-ncl1018985.nclmiami.ncl.com>
2018-05-24 14:33:13 -0400

    debugging

--- src/linuxacademy/git/gitStuff.groovy
+++ src/linuxacademy/git/gitStuff.groovy
@@ -2,8 +2,15 @@ package linuxacademy.git;
 
 def gitCommit(gitPath) {
     ///bin/git --git-dir='${gitPath}' rev-parse HEAD
-    println '/usr/bin/ls'.execute().text
+
     // echo ""
     // echo proc.text
+
+    def cmd = 'hostname'
+    def sout = new StringBuffer(), serr = new StringBuffer()
+    def proc = cmd.execute()
+    proc.consumeProcessOutput(sout, serr)
+    proc.waitForOrKill(1000)
+    println sout
     return "It ran!"
 }
