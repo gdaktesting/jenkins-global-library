a870e3664178a3e03cf6170d2eac66937ccc15f1
Dak Gonzalez <dgonzalez@dgonzalez-dev-ncl1018985.nclmiami.ncl.com>
2018-05-24 14:34:03 -0400

    debugging

--- src/linuxacademy/git/gitStuff.groovy
+++ src/linuxacademy/git/gitStuff.groovy
@@ -7,10 +7,16 @@ def gitCommit(gitPath) {
     // echo proc.text
 
     def cmd = 'hostname'
+    echo "1"
     def sout = new StringBuffer(), serr = new StringBuffer()
+    echo "2"
     def proc = cmd.execute()
+    echo "3"
     proc.consumeProcessOutput(sout, serr)
+    echo "4"
     proc.waitForOrKill(1000)
+    echo "5"
     println sout
+    echo "6"
     return "It ran!"
 }
