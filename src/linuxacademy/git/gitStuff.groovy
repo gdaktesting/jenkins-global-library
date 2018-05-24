8b5723d26fcdbdd082584d932853803f232a1d9e
Dak Gonzalez <dgonzalez@dgonzalez-dev-ncl1018985.nclmiami.ncl.com>
2018-05-24 15:01:21 -0400

    debugging

--- src/linuxacademy/git/gitStuff.groovy
+++ src/linuxacademy/git/gitStuff.groovy
@@ -1,20 +1,12 @@
 package linuxacademy.git;
 
-@Whitelisted
 def gitCommit(gitPath) {
     ///bin/git --git-dir='${gitPath}' rev-parse HEAD
 
     // echo ""
     // echo proc.text
 
-    while (true) {
-        def cmd = input message: 'What to run:', parameters: [string(defaultValue: '', description: '', name: 'cmd')]
-        try {
-            print Eval.x(this,cmd)
-        } catch (e) {
-            print e
-        }
-    }
 
-    return "It ran!"
+
+    return "ls -la".execute().text
 }
