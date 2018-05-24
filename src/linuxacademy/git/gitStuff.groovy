6ec6c0b21e08ea59021e4aeaf5a7179ae68c1a5d
Dak Gonzalez <dgonzalez@dgonzalez-dev-ncl1018985.nclmiami.ncl.com>
2018-05-24 14:11:56 -0400

    debugging

--- src/linuxacademy/git/gitStuff.groovy
+++ src/linuxacademy/git/gitStuff.groovy
@@ -2,7 +2,7 @@ package linuxacademy.git;
 
 def gitCommit(gitPath) {
     ///bin/git --git-dir='${gitPath}' rev-parse HEAD
-    // def proc = "ls".execute()
+    "ls".execute()
     // echo ""
     // echo proc.text
     return "It ran!"
