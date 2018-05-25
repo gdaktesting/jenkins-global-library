5f6e582b9c6ce59b9365b10bebbf380b9494d75f
Dak Gonzalez <dgonzalez@dgonzalez-dev-ncl1018985.nclmiami.ncl.com>
2018-05-25 11:31:34 -0400

    fixing node

--- vars/showNodeJSVersion.groovy
+++ vars/showNodeJSVersion.groovy
@@ -1,3 +1,10 @@
 def call(String nodeENV = "NodeJS_8.11.2") {
-    echo "Node: ${nodeENV}"
+    env.NODEJS_HOME = "${tool 'NodeJS_8.11.2'}"
+    // on linux / mac
+    env.PATH="${env.NODEJS_HOME}/bin:${env.PATH}"
+    // on windows
+    env.PATH="${env.NODEJS_HOME};${env.PATH}"
+    sh 'node --version'
+    sh 'npm --version'
+    sh 'gulp --version'
 }
