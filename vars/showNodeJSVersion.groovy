37045e5b04f6805c1b36833654564539f37e4374
Dak Gonzalez <dgonzalez@dgonzalez-dev-ncl1018985.nclmiami.ncl.com>
2018-05-29 10:28:26 -0400

    testing

--- vars/showNodeJSVersion.groovy
+++ vars/showNodeJSVersion.groovy
@@ -1,12 +1,25 @@
+def installTools() {
+    // Install tools
+    def nodeHome = tool(name: 'Node')
+    return [
+            "PATH+NODE=${nodeHome}/bin",
+    ]
+}
+
 def call(String nodeENV = "NodeJS_8.11.2") {
 
     node ('CentOS'){
-        env.NODEJS_HOME = "${tool 'NodeJS_8.11.2'}"
+        //env.NODEJS_HOME = "${tool 'NodeJS_8.11.2'}"
         // on linux / mac
-        env.PATH="${env.NODEJS_HOME}/bin:${env.PATH}"
-        sh "node --version"
-        sh "npm --version"
-        sh "gulp --version"
+        //env.PATH="${env.NODEJS_HOME}/bin:${env.PATH}"
+
+        withEnv(installTools()) {
+
+            sh "node --version"
+            sh "npm --version"
+            sh "gulp --version"
+
+        }
     }
 
 }
